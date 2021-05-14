8fdaf2b5f2d4c9c6a3196fe790941ae9197caf81
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-05-14 15:32:04 -0300

    Creating the concatenate function

--- challenge11.c
+++ challenge11.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int countCharacters(char word[]){
-    int length = 0;
+    int length = 1;
 
     while(word[length] != '\0'){
         length++;
@@ -10,15 +10,34 @@ int countCharacters(char word[]){
     return length;
 }
 
+void concatenateStrings(char str1[], char str2[], char result[]){
+
+
+    for(int i = 0; i < countCharacters(str1); i++){
+        if(str1[i] != '\n')
+                result[i] = str1[i];
+    }
+
+    for(int i = 0; i < countCharacters(str2); i++){
+        if(str2[i] != '\n')
+            result[countCharacters(str1)+i] = str2[i];
+    }
+}
 
 int main(){
 
-    char word[10];
+    char word1[10], word2[10], result[20];
 
     printf("Put a word: \n");
-    scanf("%[^\n]s", word);
+    /*scanf("%[^\n]s", word1);*/
+    fgets(word1, 10, stdin);
+    printf("Put a word: \n");
+    /*scanf("%[^\n]s", word2);*/
+    fgets(word2, 10, stdin);
 
-    printf("%d characters.\n", countCharacters(word));
+    concatenateStrings(word1, word2, result);
+
+    printf("%s\n", result);
 
     return 0;
 }
