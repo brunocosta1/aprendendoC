f607dbe3759ead264cda952357f29d88746001c9
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-05-26 17:28:59 -0300

    Improving the showStringInverse function

--- challenge12.c
+++ challenge12.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+
 void showStringInverse(char str1[]){
 
     int length = strlen(str1);
     char aux[length];
 
-    for(int i = 0; i < length; i++)
+    for(int i = 0; str1[i] != '\0' ; i++){
         aux[i] = str1[length-i-1];
+        aux[i+1] = '\0';
+    }
 
     printf("%s\n", aux);
 
@@ -46,20 +49,20 @@ int main(){
     char word1[10], word2[10];
     int n = 0;
 
+    scanf("%s", word1);
+    showStringInverse(word1);
+
     printf("Input number of strings: ");
     scanf("%d", &n);
+
     
     char strings[n][10];
 
     for(int i = 0; i < n; i++){
         printf("Input the string %d:", i+1);
         scanf("%s", strings[i]);
-        /*fgets(strings[i], 10, stdin);*/
         fflush(stdin);
     }
-    /*fgets(word1, 10, stdin);*/
-
-    /*showStringInverse(word1);*/
 
     bubbleSortString(n, strings);
     return 0;
